f31d0977c6f8ab3736ae705217d0cad72f516c51
SargentoPimienta01 <103974196+SargentoPimienta01@users.noreply.github.com>
2023-11-14 15:04:54 -0400

    Cajas Estadarizadas CSS

--- resources/views/home/cajas/cajas.blade.php
+++ resources/views/home/cajas/cajas.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.indexbutton')
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,31 +8,49 @@
     <meta content="Product Design, Product Management and Webflow Development. I design thoughtful user experiences that piece together a big picture with simple, impactful and shippable solutions focused on the customer" name="description"/>
     <title>Cajas | Univalle</title>
     <link href="{{ Vite::asset('resources/css/styles.css') }}" rel="stylesheet" type="text/css"/>
-    <link href="{{ Vite::asset('resources/css/tramites/styles_user_tables.css') }}" rel="stylesheet" type="text/css"/>
-    <link rel="shortcut icon" type="image/png" href="{{ Vite::asset('resources/css/styles.css') }}">
+    <link href="{{ Vite::asset('resources/css/menu.css') }}" rel="stylesheet" type="text/css"/>
+    <link rel="shortcut icon" type="image/png" href="{{ Vite::asset('resources/img/UnivalleLogo.png') }}">
+    
 </head>
 <body>
   
-    <div class="titulo">
-        <h2>Cajas</h2>
-    </div>
-      
-    <aside>
-        <nav>
-            <ul>
-                @foreach($cajas as $caja)
-                    <li>
-                        <form method="POST" action="{{ route('requisitosCaja') }}">
-                            @csrf
-                            <input type="hidden" name="id_caja" value="{{ $caja->Id_caja }}">
-                            <!-- Agrega el resto de los campos del formulario si es necesario -->
-                            <button type="submit" class="custom-button">{{ $caja->nombre_caja }}</button>
-                        </form>
-                    </li>
-                @endforeach
-            </ul>
-        </nav>
-    </aside>
+    <div class="intro">
+        <h1>Tramites</h1>
+      </div>
+  
+      <div class="hero">
+          <!-- Barra de Navegacion -->
+          <nav>
+              <a href="{{ url('/home') }}">
+              <img src="{{ Vite::asset('resources/img/banner.png') }}" alt="Univalle Logo" class="logo">
+              </a>
+              <ul>
+                  <li><a href="{{ url('/home') }}">Menu</a></li>
+                  <li><a href="{{ url('/home/tramites') }}">Tramites</a></li>
+                  <li><a href="{{ url('/home/cajas') }}">Cajas</a></li>
+                  <li><a href="#">Postgrado</a></li>
+              </ul>
+              <button class="buttonS" type="button"><a href="{{ route('login') }}">Inicio de Sesion</a></button>
+          </nav>
+
+          <div class="Opciones2">
+            @foreach($cajas as $caja)
+                    
+                <form method="POST" action="{{ route('requisitosCaja') }}">
+                    @csrf
+                        <input type="hidden" name="id_caja" value="{{ $caja->Id_caja }}">
+                        <!-- Agrega el resto de los campos del formulario si es necesario -->
+                        <button type="submit" class="button-anon-pen"><span>{{ $caja->nombre_caja }}</span></button>
+                </form>    
+                
+            @endforeach
+
+        </div>
         
+
+
+    </div>
+
+    <script src="{{ Vite::asset('resources/js/intro.js') }}"></script>    
 </body>
 </html>
--- resources/views/home/cajas/requisitos.blade.php
+++ resources/views/home/cajas/requisitos.blade.php
@@ -1,5 +1,3 @@
-@extends('layouts.indexbutton')
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,43 +6,90 @@
     <meta content="summary_large_image" name="twitter:card"/>
     <meta property="og:type" content="website"/>
     <meta content="Product Design, Product Management and Webflow Development. I design thoughtful user experiences that piece together a big picture with simple, impactful and shippable solutions focused on the customer" name="description"/>
-    <title>Cajas | Univalle</title>
-    <link href="{{ Vite::asset('resources/css/tramites/styles.css') }}" rel="stylesheet" type="text/css"/>
+    <title>Tramites | Univalle</title>
+    <link href="{{ Vite::asset('resources/css/styles.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ Vite::asset('resources/css/menu.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ Vite::asset('resources/css/cards.css') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ Vite::asset('resources/css/tramites/styles_user_tables.css') }}" rel="stylesheet" type="text/css"/>
-    <link rel="shortcut icon" type="image/png" href="{{ Vite::asset('resources/css/styles.css') }}">
+    <link rel="shortcut icon" type="image/png" href="{{ Vite::asset('resources/img/UnivalleLogo.png') }}">
     
 </head>
 <body>
-  
-    <div class="titulo">
-      <h2>Requisitos para la caja: {{ $requisitoCaja->nombre_caja }}</h2>
-    </div>
 
+    <div class="intro">
+        <h1>{{ $nombreTramite }}</h1>
+      </div>
+
+      <div class="hero">
+        <!-- Barra de Navegacion -->
+        <nav>
+            <a href="{{ url('/home') }}">
+            <img src="{{ Vite::asset('resources/img/banner.png') }}" alt="Univalle Logo" class="logo">
+            </a>
+            <ul>
+                <li><a href="{{ url('/home') }}">Menu</a></li>
+                <li><a href="{{ url('/home/tramites') }}">Tramites</a></li>
+                <li><a href="{{ url('/home/cajas') }}">Cajas</a></li>
+                <li><a href="#">Postgrado</a></li>
+            </ul>
+            <button class="buttonS" type="button"><a href="{{ route('login') }}">Inicio de Sesion</a></button>
+        </nav>
+
+        <div>
+            <h2 style="color: white; text-align: center;">Requisitos para el trámite: {{ $nombreTramite }}</h2>
+        </div>
+
+
+
+        <div class ="contenidopro">
+
+            @foreach ($requisitos as $requisito)
+
+            <div class="">
+            <div class="card">
+                <div class="face front">
+                    <img src="{{ Vite::asset('resources/img/tramites/tramite.jpg') }}" alt="Requisitos">
+                    <h3>Requisitos</h3>
+                </div>
+                <div class="face back">
+                    <h3>Requisitos</h3>
+                    <p>{{ $requisito->descripcion_requisito }}</p>
+                    
+                </div>
+            </div>
+            </div>
     
-    <div class="container">
-        <table>
-            <thead>
-                <tr>
-                    <th>Servicio</th>
-                    <th>Requisitos</th>  
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>SER</td>
-                    <td>REQ</td>
-                </tr>
-                
-            </tbody>
-            <tbody>
-                @foreach ($requisitos as $requisito)
-                <tr>
-                    <td>{{ $requisito->nombre_requisito }}</td>
-                    <td>{{ $requisito->descripcion_requisito }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-</div>
+            <div class="card">
+                <div class="face front">
+                    <img src="{{ Vite::asset('resources/img/tramites/duracion.jpeg') }}" alt="Duracion">
+                    <h3>Duracion</h3>
+                </div>
+                <div class="face back">
+                    <h3>Duracion</h3>
+                    <p>{{ $duracionTramite }}</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="face front">
+                    <img src="{{ Vite::asset('resources/img/tramites/ubicacion.jpeg') }}" alt="">
+                    <h3>Ubicacion</h3>
+                </div>
+                <div class="face back">
+                    <h3>Ubicacion</h3>
+                    <p>Profesionales en esta área trabajan en el diseño de estrategias de gestión y políticas económicas. Su labor contribuye a mejorar la toma de decisiones, promoviendo un entorno económico y empresarial más efectivo y sostenible.</p>
+                    
+                </div>
+            </div>
+    
+            @endforeach
+        </div>
+        
+
+
+        
+    </div>  
+
+    <script src="{{ Vite::asset('resources/js/intro.js') }}"></script>
 </body>
 </html>
