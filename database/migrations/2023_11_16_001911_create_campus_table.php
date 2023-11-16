d23e25e783c398e95db9eee5946d99261c46362e
Ian Martinez <145503631+bIacklol597@users.noreply.github.com>
2023-11-16 00:29:54 -0400

    Add files via upload

---
+++ database/migrations/2023_11_16_001911_create_campus_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('campus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->time('hora'); // Assuming 'hora' is a time field, change it accordingly
+            $table->date('fecha');
+            $table->integer('estado')->default(1);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bibliotecas');
+    }
+};
