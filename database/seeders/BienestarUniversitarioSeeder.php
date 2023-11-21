ecbbca430f4b99b2cc9ed7a02d660336f004d713
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-11-21 08:03:36 -0400

    Gabinetes de bienestar

--- database/seeders/BienestarUniversitarioSeeder.php
+++ database/seeders/BienestarUniversitarioSeeder.php
@@ -22,6 +22,38 @@ class BienestarUniversitarioSeeder extends Seeder
             'created_at' => now(),
             'updated_at' => now(),
         ]);
+        DB::table('bienestar_universitario')->insert([
+            'servicio' => 'Gabinete psicológico',
+            'detalle' => 'Presentación de credencial universitaria',
+            'estado' => 1,
+            'Id_area' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('bienestar_universitario')->insert([
+            'servicio' => 'Consultorio Jurídico',
+            'detalle' => 'Presentación de credencial universitaria',
+            'estado' => 1,
+            'Id_area' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('bienestar_universitario')->insert([
+            'servicio' => 'CAE - Asesoramiento y acompañamiento a estudiantes',
+            'detalle' => 'Ser estudiante de Univalle - La Paz',
+            'estado' => 1,
+            'Id_area' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        DB::table('bienestar_universitario')->insert([
+            'servicio' => 'Vitrina de descuentos Univalle',
+            'detalle' => 'Presentación de credencial universitaria',
+            'estado' => 1,
+            'Id_area' => 7,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
 }
