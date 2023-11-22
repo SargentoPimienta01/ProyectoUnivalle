b593f469b8ad0ec1dcaf399041988ef0c3c5ad1b
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-11-22 12:08:27 -0400

    Vamooos, que se puedeee por favor

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 import './bootstrap';
+import.meta.glob([
+    '../css/gabinetemedicoynaf/**',
+    '../css/plataformadeatencion/**',
+    '../css/tramites/**',
+    '../css/**',
+    '../css/**',
+    '../js/**',
+    '../images/**',
+    '../fonts/**',
+  ]);
