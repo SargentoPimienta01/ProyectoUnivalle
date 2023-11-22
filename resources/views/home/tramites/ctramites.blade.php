72be88b7665d5d31973bda88e51722602876e087
kinauw3 <kinnx64@gmail.com>
2023-11-21 20:03:50 -0400

    tipos de tramites

--- resources/views/home/tramites/ctramites.blade.php
+++ resources/views/home/tramites/ctramites.blade.php
@@ -7,30 +7,23 @@
     <meta property="og:type" content="website"/>
     <meta content="Product Design, Product Management and Webflow Development. I design thoughtful user experiences that piece together a big picture with simple, impactful and shippable solutions focused on the customer" name="description"/>
     <title>Tramites | Univalle</title>
-    <link href="{{ Vite::asset('resources/css/styles.css') }}" rel="stylesheet" type="text/css"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
     <link href="{{ Vite::asset('resources/css/menu.css') }}" rel="stylesheet" type="text/css"/>
     <link rel="shortcut icon" type="image/png" href="{{ Vite::asset('resources/img/UnivalleLogo.png') }}">
-    
 </head>
 <body>
-  
     <div class="hero">
         <!-- Barra de Navegacion -->
         <nav>
             <a href="{{ url('/home') }}">
             <img src="{{ Vite::asset('resources/img/banner.png') }}" alt="Univalle Logo" class="logo">
             </a>
-            <ul>
-                <li><a href="{{ url('/home') }}">Menu</a></li>
-                <li><a href="{{ url('/home/tramites') }}">Tramites</a></li>
-                <li><a href="{{ url('/home/cajas') }}">Cajas</a></li>
-                <li><a href="#">Postgrado</a></li>
-            </ul>
+           
             <button class="buttonS" type="button"><a href="{{ route('login') }}">Inicio de Sesion</a></button>
         </nav>
 
         <div>
-            <h2 style="color: white; text-align: center;">Tramites</h2>
+            <h2 style="color: white; text-align: center;">Tipos de tramites</h2>
         </div>
 
         <div class="Opciones2">
@@ -40,14 +33,7 @@
                 </a>
             @endforeach
         </div>
-
-        
-
-
-
-    </div>
-    
-        
+    </div>  
       <script src="{{ Vite::asset('resources/js/intro.js') }}"></script>
 </body>
 </html>
