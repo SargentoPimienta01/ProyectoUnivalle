0cedaed92d844c0840ccd56f71d7093be1aecb2d
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-12-04 12:41:08 -0400

    Seeeders plataforma de atencion

--- database/seeders/PlataformaDeAtencionSeeder.php
+++ database/seeders/PlataformaDeAtencionSeeder.php
@@ -15,18 +15,58 @@ class PlataformaDeAtencionSeeder extends Seeder
     public function run()
     {
         PlataformaDeAtencion::create([
-            'servicio' => 'Soporte Técnico',
+            'servicio' => 'Plan de pagos',
             'descripcion' => 'Brindamos asistencia técnica especializada',
-            'requisitos' => 'Requisito 1',
+            'requisitos' => 'Acceso a Netvalle. El estudiante debe haber cancelado 1000bs de inscripción.',
+            'estado' => true,
+            'Id_area' => 9,
+        ]);
+
+        PlataformaDeAtencion::create([
+            'servicio' => 'Inscripción de estudiantes nuevos',
+            'descripcion' => 'Brindamos asistencia técnica especializada',
+            'requisitos' => 'Plataforma inscribe en sistema Netvalle solo a estudiantes nuevos. Se necesitan: Fotocopia de CI, 2 Fotocopias legalizadas de Título de Bachiller, 2 Certificados de nacimiento.',
             'estado' => true,
             'Id_area' => 9,
         ]);
     
         PlataformaDeAtencion::create([
-            'servicio' => 'Consulta General',
+            'servicio' => 'Información de carreras',
             'descripcion' => 'Atención para consultas y preguntas generales',
-            'requisitos' => 'Requisito 2',
-            'estado' => false,
+            'requisitos' => 'Plataforma necesita acceso con montos aprobados y oficiales',
+            'estado' => true,
+            'Id_area' => 9,
+        ]);
+
+        PlataformaDeAtencion::create([
+            'servicio' => 'Reimpresión de credenciales',
+            'descripcion' => 'Atención para consultas y preguntas generales',
+            'requisitos' => 'Comprobante de pago o factura de Bs. 70, dos días para la impresión. Identificación del estudiante.',
+            'estado' => true,
+            'Id_area' => 9,
+        ]);
+
+        PlataformaDeAtencion::create([
+            'servicio' => 'Solvencia',
+            'descripcion' => 'Atención para consultas y preguntas generales',
+            'requisitos' => 'Estado económico sin deudas del estudiante, Plataforma de acceso a Netvalle.',
+            'estado' => true,
+            'Id_area' => 9,
+        ]);
+
+        PlataformaDeAtencion::create([
+            'servicio' => 'Reservas',
+            'descripcion' => 'Atención para consultas y preguntas generales',
+            'requisitos' => 'El sistema de plataforma debe estar habilitado para la siguiente gestión. El estudiante debe presentar su CI y un pago mínimo para la siguiente gestión.',
+            'estado' => true,
+            'Id_area' => 9,
+        ]);
+
+        PlataformaDeAtencion::create([
+            'servicio' => 'Las inscripciones desde segundo semestre',
+            'descripcion' => 'Atención para consultas y preguntas generales',
+            'requisitos' => 'Se deben realizar en el sistema o en cajas directamente.',
+            'estado' => true,
             'Id_area' => 9,
         ]);
     }
