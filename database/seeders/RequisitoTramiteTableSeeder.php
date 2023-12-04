111f8ef2ae0a6500f8c631bab4ff46a8062b3344
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-12-04 11:19:29 -0400

    Seeders de certificados

--- database/seeders/RequisitoTramiteTableSeeder.php
+++ database/seeders/RequisitoTramiteTableSeeder.php
@@ -17,7 +17,72 @@ class RequisitoTramiteTableSeeder extends Seeder
                 'estado' => true,
                 'Id_tramite' => 1, // Cambia esto según el ID del trámite correspondiente
             ],
-            // Agrega más requisitos según sea necesario
+            [
+                'nombre_requisito' => 'Certificado de Culminación de Plan de Estudios. ',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 2, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Certificado de Vencimiento de Plan de Estudios. ',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 3, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Certificado de Puntaje. ',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 4, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Certificado de Carga de Horaria (Horas reloj y Académicas). ',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 5, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Nomina de Docentes.',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 6, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Historial Académico. ',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 7, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Estudios en Hora Reloj.',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 8, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Certificado Promedio.',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 9, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Orden de Mérito o Ranking.',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 10, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Internado Rotatorio (Urbano y Rural).',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 11, // Cambia esto según el ID del trámite correspondiente
+            ],
+            [
+                'nombre_requisito' => 'Informe Técnico del S.S.S.R.O.',
+                'descripcion_requisito' => "1. Solicitar el Formulario de Certificado, otorgado por ventanilla de trámites o presentar una carta (1 ejemplar) de solicitud al VICERRECTOR de la Universidad (M. Sc. Ing. Franklin Nestor Rada).\n2. Indicar a dónde o a quién debe ir dirigido dicho certificado (Empresa, Institución).\n3. Estado Económico, solicitado en Plataforma de Informaciones.",
+                'estado' => true,
+                'Id_tramite' => 12, // Cambia esto según el ID del trámite correspondiente
+            ],
         ];
 
         // Inserta los datos en la tabla "RequisitoTramite" para el trámite 1
