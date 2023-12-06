3335fd859b129e0329a765636531c05e8b39d748
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-12-06 09:57:53 -0400

    deploy12

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,11 +1,5 @@
 import './bootstrap';
 import.meta.glob([
-    '../js/**',
     '../images/**',
     '../img/**',
-    '../fonts/**',
-    '../css/**',
-    '../css/gabinetemedicoynaf/**',
-    '../css/plataformadeatencion/**',
-    '../css/tramites/**',
   ]);
