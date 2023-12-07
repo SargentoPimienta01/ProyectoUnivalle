bbab0f2caf3ab23a75b94607a92e86a9beaaf7a0
Jpmc260802UV <103669140+JPMenachUVal@users.noreply.github.com>
2023-12-06 23:30:26 -0400

    deploy19

--- app/Models/producto.php
+++ app/Models/producto.php
@@ -4,15 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Producto extends Model
 {
     use HasFactory;
-    use SoftDeletes;
-    protected $view_deleted = 'DeletedRecords';
     protected $fillable = [ 'nombre', 'descripcion', 'precio', 'foto', 'id_categoria', 'estado'];
-    protected $dates = ['deleted_at']; 
 
     public function categoria()
     {
