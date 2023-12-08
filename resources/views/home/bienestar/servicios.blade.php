067bfc64eef2629b7da68ca933ea612640fbd122
kinauw3 <kinnx64@gmail.com>
2023-12-08 10:49:48 -0400

    Bienestar_v2_principal

--- resources/views/home/bienestar/servicios.blade.php
+++ resources/views/home/bienestar/servicios.blade.php
@@ -1,68 +1,138 @@
-
 @extends('layouts.backspace')
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="summary_large_image" name="twitter:card"/>
-    <meta property="og:type" content="website"/>
+    <meta content="summary_large_image" name="twitter:card" />
+    <meta property="og:type" content="website" />
     <title>Cajas | Univalle</title>
     <link rel="stylesheet" href="{{ Vite::asset('resources/css/nav.css') }}">
-    <link href="{{ Vite::asset('resources/css/cards.css') }}" rel="stylesheet" type="text/css"/>
     <link rel="shortcut icon" type="image/png" href="{{ Vite::asset('resources/images/UnivalleLogo.png') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-   
-    
+    <style>
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .contenidopro {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+        }
+
+        .card {
+            width: 300px;
+            height: 100%;
+            padding: 10PX;
+            border: 1px solid #ddd;
+            border-radius: 8px;
+            transition: transform 0.3s ease-in-out;
+            box-shadow: 0 4px 8px rgba(128, 9, 9, 0.945);
+            margin-bottom: 20px;
+            display: flex;
+            flex-direction: column;
+            margin-right: 10px; /* Add margin here */
+        }
+
+        .card:hover {
+            transform: scale(1.01);
+        }
+
+        .card-body {
+            padding: 20px;
+            text-align: left;
+        }
+
+        .card-title {
+            font-size: 18px;
+            margin-bottom: 10px;
+        }
+
+        .card-text {
+            font-size: 14px;
+            line-height: 1.5;
+            white-space: pre-line;
+        }
+
+        .btn-custom {
+            background-color: rgb(90, 18, 18);
+            color: white;
+            padding: 10px 15px;
+            text-decoration: none;
+            border-radius: 5px;
+            display: inline-block;
+            transition: background-color 0.3s ease;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: auto;
+        }
+
+        .btn-custom:hover {
+            background-color: #631212;
+            color: white;
+        }
+
+        .contact-info {
+            margin-top: auto;
+        }
+
+        .contact-link {
+            color: inherit;
+            text-decoration: underline;
+        }
+
+        .modal-content {
+            height: 90vh;
+        }
+
+        .modal-body {
+            max-height: 80vh;
+            overflow-y: auto;
+        }
+
+        iframe {
+            width: 100%;
+            height: 100%;
+            border: none;
+            transform: scale(0.9);
+        }
+    </style>
 </head>
+
 <body>
-
-      <div class="hero">
-        <!-- Barra de Navegacion -->
-        <!--<nav>
-            <a href="{{ url('/home') }}">
-            <img src="{{ Vite::asset('resources/img/banner.png') }}" alt="Univalle Logo" class="logo">
-            </a>
-            <ul>
-                <li><a href="{{ url('/home') }}">Menu</a></li>
-                <li><a href="{{ url('/home/tramites') }}">Tramites</a></li>
-                <li><a href="{{ url('/home/cajas') }}">Cajas</a></li>
-                <li><a href="#">Postgrado</a></li>
-            </ul>
-            <button class="buttonS" type="button"><a href="{{ route('login') }}">Inicio de Sesion</a></button>
-        </nav>-->
-
-        <div>
-            <h2 style="color: white; text-align: center;">Requisitos de Servicio: {{ $servicioBienestar->servicio }}</h2>
+    <div class="hero">
+        <div style="color: #8B0000; text-align: center; margin-top: 20px; margin-bottom: 40px;">
+            <h2>Requisitos de Servicio {{ $servicioBienestar->servicio }}</h2>
         </div>
+        
 
-
-
-        <div class ="contenidopro">
-
+        <div class="contenidopro">
             @foreach ($servicios as $servicio)
-
-            <div class="">
             <div class="card">
                 <div class="face front">
-                    <img src="{{ Vite::asset('resources/img/tramites/tramite.jpg') }}" alt="Requisitos">
-                    <h3>Servicio</h3>
+                    <img src="{{ Vite::asset('resources/img/tramites/tramite.jpg') }}" alt="Requisitos" class="img-fluid">
+                    
                 </div>
                 <div class="face back">
                     <h3>Servicio</h3>
                     <p>{{ $servicio->servicio }}</p>
-                    
                 </div>
             </div>
-            </div>
-    
+
             <div class="card">
                 <div class="face front">
-                    <img src="{{ Vite::asset('resources/img/tramites/duracion.jpeg') }}" alt="Duracion">
-                    <h3>Requisitos</h3>
+                    <img src="{{ Vite::asset('resources/img/tramites/duracion.jpeg') }}" alt="Duracion" class="img-fluid">
+                    
                 </div>
                 <div class="face back">
                     <h3>Requisitos</h3>
@@ -72,24 +142,19 @@
 
             <div class="card">
                 <div class="face front">
-                    <img src="{{ Vite::asset('resources/img/tramites/duracion.jpeg') }}" alt="Duracion">
-                    <h3>Ubicación</h3>
+                    <img src="{{ Vite::asset('resources/img/tramites/duracion.jpeg') }}" alt="Duracion" class="img-fluid">
+                    
                 </div>
                 <div class="face back">
-                    <h3>Requisitos</h3>
+                    <h3>Ubicación</h3>
                     <p>{{ $ubicacion->nombre_ubicacion }}</p>
                 </div>
             </div>
-
-    
             @endforeach
         </div>
-        
-
-
-        
-    </div>  
+    </div>
 
     <script src="{{ Vite::asset('resources/js/intro.js') }}"></script>
 </body>
+
 </html>
--- resources/views/home/plataforma-de-atencion/index.blade.php
+++ resources/views/home/plataforma-de-atencion/index.blade.php
@@ -1,61 +1,169 @@
 @extends('layouts.backspace')
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ Vite::asset('resources/css/nav.css') }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ Vite::asset('resources/css/plataformadeatencion/estilos.css') }}" rel="stylesheet" type="text/css"/>
     <title>Plataforma de Atención</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            color: black;
+        }
+
+        header {
+            background-color: #6e1010;
+            color: rgb(102, 14, 14);
+            text-align: center;
+            padding: 10px;
+        }
+
+        nav ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            background-color: #6d1515;
+            padding: 10px;
+        }
+
+        nav ul li {
+            margin: 0 10px;
+        }
+
+        nav ul li a {
+            text-decoration: none;
+            color: blue;
+        }
+
+        #servicios-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+        }
+
+        .toggle-button {
+            color: black;
+            text-decoration: none;
+            display: inline-block;
+            padding: 15px 20px;
+            background-color: #eee;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            transition: background-color 0.3s;
+            font-size: 14px;
+            margin: 10px auto;
+        }
+
+        .card,
+        .service-card {
+            margin: 10px;
+            width: calc(90% - 20px);
+            padding: 10px;
+            text-align: center;
+            background: var(--transparent-white);
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(128, 9, 9, 0.945);
+            transition: transform 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
+        }
+
+        .image-card img {
+            width: 100%; 
+            height: auto; 
+            max-width: 300px; 
+            max-height: 200px; 
+        }
+
+        .image-card {
+            margin: 10px;
+            padding: 10px;
+            text-align: center;
+            background: var(--transparent-white);
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(128, 9, 9, 0.945);
+            transition: transform 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
+        }
+
+
+        .service-card {
+            width: calc(20% - 20px);
+            display: none;
+        }
+
+        .service-card.active {
+            display: block;
+            opacity: 1;
+            height: auto;
+        }
+
+        .service-card:hover,
+        .image-card:hover {
+            opacity: 1;
+            transform: scale(1.1); 
+        }
+
+        button {
+            background-color: #333;
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            font-size: 16px;
+        }
+
+        #servicios h3 {
+            color: rgb(0, 0, 0);
+        }
+    </style>
 </head>
+
 <body>
-    <header>
-        <h1>Plataforma de Atención</h1>
-        <nav>
-            <ul>
-                <li><a href="{{ route('home') }}">Inicio</a></li>
-                <li><a href="{{ route('paservicios') }}">Servicios</a></li>
-                <!--<li><a href="#contacto">Contacto</a></li>-->
-            </ul>
-        </nav>
-    </header>
+    <h1 style="text-align: center; color: maroon;">Plataforma de Atención</h1>
 
-    <section id="inicio">
-        <h2>Bienvenido a nuestra plataforma de atención</h2>
-        <p>Ofrecemos servicios de atención al cliente de alta calidad.</p>
-        <button>Conoce más</button>
+    <section id="servicios-container">
+<!-- Card box con imagen 1 -->
+<div class="image-card">
+    <a href="https://belltech.la/wp-content/uploads/2022/12/Software-y-plataforma-de-atencion-al-cliente-Ventajas-para-tu-empresa-2.jpg" target="_blank">
+        <img src="https://belltech.la/wp-content/uploads/2022/12/Software-y-plataforma-de-atencion-al-cliente-Ventajas-para-tu-empresa-2.jpg" alt="Descripción de la imagen 1" style="max-width: 100%; height: auto;">
+    </a>
+</div>
 
-       
+<!-- Card box con imagen 2 -->
+<div class="image-card">
+    <a href="https://cdn.www.gob.pe/uploads/document/file/2999129/standard_WhatsApp%20Image%202022-04-06%20at%204.41.45%20PM.jpeg.jpeg" target="_blank">
+        <img src="https://cdn.www.gob.pe/uploads/document/file/2999129/standard_WhatsApp%20Image%202022-04-06%20at%204.41.45%20PM.jpeg.jpeg" alt="Descripción de la imagen 2" style="max-width: 100%; height: auto;">
+    </a>
+</div>
+
+<div class="card">
+    <h3>Nuestros Servicios</h3>
+    <p>Explora nuestras opciones de servicios, que incluyen asesoramiento académico, información sobre eventos y actividades, acceso a material educativo, y mucho más. Nos esforzamos por proporcionar un ambiente virtual que complemente y enriquezca tu vida estudiantil, asegurando que encuentres las herramientas y la asistencia necesarias en un solo lugar.</p>
+    <button onclick="toggleServices()" class="toggle-button">Servicios</button>
+</div>
+
+@foreach($plataformasdeatencion as $servicio)
+<div class="service-card hidden">
+    <h3>{{ $servicio->servicio }}</h3>
+    <p>{{ $servicio->descripcion }}</p>
+</div>
+@endforeach
+
     </section>
 
-    <section id="servicios">
-        <h2>Nuestros Servicios</h2>
-        <ul>
-            <!--<li>Atención al Cliente</li>
-            <li>Soporte Técnico</li>
-            <li>Asesoramiento Personalizado</li>-->
-            @foreach($plataformasdeatencion as $servicio)
-            <li>{{ $servicio->servicio}}</li>
-            @endforeach
-        </ul>
-    </section>
-
-    <!--<section id="contacto">
-        <h2>Contacto</h2>
-        <p>Estamos aquí para ayudarte. Contáctanos:</p>
-        <br>
-        <form>
-            <label for="nombre">Nombre:</label>
-            <input type="text" id="nombre" name="nombre" required>
-
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-
-            <button type="submit">Enviar Mensaje</button>
-        </form>
-    </section>-->
-    <!--<footer>
-        <p>&copy; 2023 Plataforma de Atención. Todos los derechos reservados.</p>
-    </footer>-->
+    <script>
+        function toggleServices() {
+            var serviceCards = document.querySelectorAll('.service-card');
+            serviceCards.forEach(function(card) {
+                card.classList.toggle('active');
+            });
+        }
+    </script>
 </body>
 </html>
